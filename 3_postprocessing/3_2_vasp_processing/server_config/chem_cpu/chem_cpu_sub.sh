c4400f028add3fd39f18ff6685713f3b519fc86c
Bagabaga007 <49644513+Bagabaga007@users.noreply.github.com>
2025-04-02 15:20:14 +0800

    Delete 3_postprocessing directory

--- 3_postprocessing/3_2_vasp_processing/server_config/chem_cpu/chem_cpu_sub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -J yz_vasp
-#SBATCH -p phys
-#SBATCH -N 1
-#SBATCH -n 56
-
-source /public/env/intel2021
-export PATH=/public/software/vasp.6.3.0/bin:$PATH
-srun --mpi=pmi2 vasp_std > vasp.log 2>&1
